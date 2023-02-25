30f4c53bc5c41090ab49ab2a425c9b797f2d542a
Salah eddine Elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-02-25 19:51:53 +0100

    edit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 App_Key="e9c25421"
 App_Secret="6QjExxHWCYK8qsM0"
-App_bot_id="6250683356:AAEfpBia-twQDMONAxx0yMawOohn3PHo7CI"
+App_bot_id="6036328544:AAFvhf61ad2TLCC5Gc-iv3QV_GV0wvi1C-0"
 App_chat_id="644091115"
 App_sms_content="hello I am salah bot"
 App_from_name="from sms bot"
